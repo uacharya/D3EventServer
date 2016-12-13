7c29017b6556cbbc0f83cb8df2b198807e751385
Ujjwal Acharya <uzwal47@gmail.com>
2016-12-13 16:20:58 -0700

    The server side code for handling d3 events from each open browser running same d3 page is now updated. Now it sends dimension based on IP address of the browser computer that is requesting something and the x coordinate of the browser when made full screen.

--- D3/src/Bilevel.java
+++ D3/src/Bilevel.java
@@ -23,19 +23,29 @@ public class Bilevel {
 	// list of all the sessions into one container
 	private static final Set<Session> allSessions = Collections.synchronizedSet(new HashSet<Session>());
 
+	/**
+	 * This method handles the incoming connection from d3 web page to the d3 server
+	 * @param session started by a d3 webpage
+	 */
 	@OnOpen
 	public void handleOpen(Session session) {
 		// adding all of the sessions connected
 		allSessions.add(session); // add all the new sessions into one set
 		System.out.println("The number of client connected are " + allSessions.size());
 	}
+	
 
+	/**
+	 * This method responds to each or all of the open browsers based on the type of message sent from either one of them
+	 * @param session specific to a connection from a browser running a d3 page
+	 * @param message sent by either one of the browser or d3 page
+	 */
 	@OnMessage
 	public void handleMessage(Session session, String message) {
-		// getting request object from web filter used for the server
+		// getting ip address of the browser that has sent a message from web filter used for the server
 		String IPAddress = session.getRequestParameterMap().get("IPAddress").get(0);
 		System.out.println("the ip address for this connection is " + IPAddress);
-		// message recieved from the client browser
+		// message received from either one of the client browser
 		System.out.println("recieved event from the client is " + message);
 		// send dimension if dimension coordinates are requested for browser
 		// belonging to screen in particular node
@@ -47,6 +57,10 @@ public class Bilevel {
 
 	}
 
+	/**
+	 * This method sends event data to all the browsers that has open connection with the server
+	 * @param message event data to send
+	 */
 	private void sendMessageToAll(String message) {
 		// iterate over all active sessions and send the same event
 		for (Session ssn : allSessions) {
@@ -60,6 +74,13 @@ public class Bilevel {
 		}
 	}
 
+	
+	/**
+	 * This method creates and sends dimension data for each browser based on where the connection comes from
+	 * @param session specific to a connection from a browser running a d3 page
+	 * @param IPAddress of the browser which is requesting for dimension data
+	 * @param monitorLocation screenX coordinate of the browser which is requesting dimension data
+	 */
 	private void sendDisplayDimension(Session session, String IPAddress, String monitorLocation) {
 
 		// getting parameter for the particular browser instance and sending it
@@ -73,6 +94,13 @@ public class Bilevel {
 		}
 	}
 
+	
+	/**
+	 * This method creates and returns dimension data based on the connection Ip address and the monitor screenX location
+	 * @param IPAddress of the browser which is requesting for dimension data
+	 * @param monitorLocation screenX coordinate of the browser which is requesting dimension data
+	 * @return the required dimension data
+	 */
 	private String determineParametersToReturn(String IPAddress, String monitorLocation) {
 		String dimension = "";
 		if (IPAddress.equalsIgnoreCase("192.168.10.3")) {
@@ -119,12 +147,22 @@ public class Bilevel {
 		}
 	}
 
+	/**
+	 * This method closes the session if the web page is closed in the browser
+	 * @param session specific to a connection from a browser running a d3 page
+	 */
 	@OnClose
 	public void handleClose(Session session) {
 		System.out.println("client is now closed with ID " + session.getId());
 		allSessions.remove(session);
 	}
+	
+	
 
+	/**
+	 * This method prints stack trace if some kind of unwanted error occurred while running server
+	 * @param t the error object
+	 */
 	@OnError
 	public void handleError(Throwable t) {
 		t.printStackTrace();

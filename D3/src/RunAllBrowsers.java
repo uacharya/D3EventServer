91dd9ee1cac9be300730e368d1c6e77d319b3776
Ujjwal Acharya <uzwal47@gmail.com>
2018-02-26 18:37:24 -0700

    fixed the performance leak

--- D3/src/RunAllBrowsers.java
+++ D3/src/RunAllBrowsers.java
@@ -135,12 +135,12 @@ public class RunAllBrowsers {
 				ExecuteCommand execute = new ExecuteCommand(commandLineArgs, String.format("%s%s", j, i));
 				Thread node = new Thread(execute, execute.getThreadName());
 				node.start();
-//				try {
-//					Thread.sleep(50);
-//				} catch (InterruptedException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 
 		}
